06d95be05fae2c04466f2cbadac7114395ccffe9
Yernur <kanaternur33@gmail.com>
2026-01-30 01:01:43 +0900

    ProjectZero Update: server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'templates')));
+app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'templates', 'index.html')); });
+app.listen(PORT, () => { console.log(`> Access: http://localhost:${PORT}`); });
